1d24fa3495b4b97d09202d1861a9ec12914de666
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-23 02:52:24 +0600

    init all variabe Publicly

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
+    TextView en1,en2;
+    EditText tv;
+    Button add,minus,mul,div;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
