00d12decc96587b956295699314f91c6278604c5
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2022-02-23 03:05:55 +0600

    just java file er sate xml file id er maddhome connect kora hoyese

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -7,13 +7,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView en1,en2;
-    EditText tv;
+    TextView tv;
+    EditText en1,en2;
     Button add,minus,mul,div;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        en1= findViewById(R.id.eNumber1);
+        en2= findViewById(R.id.eNumber2);
+        tv= findViewById(R.id.eNumber3);
+        add= findViewById(R.id.add);
+        minus= findViewById(R.id.minus);
+        mul= findViewById(R.id.mul);
+        div= findViewById(R.id.divide);
 
         //this is java backend file
     }
